9c23c94688b6f5578d05b24af16ff1582f35dd37
Michael Carroll <mjcarroll@intrinsic.ai>
2024-10-01 11:07:39 -0500

    Support Bazel 6 and 7 (#2)
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -11,11 +11,29 @@ TAG=${GITHUB_REF_NAME}
 PREFIX="rules_gazebo-${TAG}"
 ARCHIVE="rules_gazebo-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
-cat << EOF
+cat > release_notes.txt << EOF
+## Using Bzlmod
+
 Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
 bazel_dep(name = "rules_gazebo", version = "${TAG}")
+\`\`\`
+
+## Using WORKSPACE
+
+Paste this snippet into your \`WORKSPACE\` file:
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "rules_gazebo",
+    sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
+    url = "https://github.com/gazebosim/rules_gazebo/releases/download/${TAG}/rules_gazebo-${TAG}.tar.gz",
+)
 \`\`\`
 EOF
